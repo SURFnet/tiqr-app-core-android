3842900aba26aa36f4d4f2c35f1c2317b4834e2c
Dániel Zolnai <zoiddani@gmail.com>
2022-10-20 10:44:55 +0200

    TIQR-308: Fix issues with migration, better logging

--- data/src/main/java/org/tiqr/data/database/TiqrDatabase.kt
+++ data/src/main/java/org/tiqr/data/database/TiqrDatabase.kt
@@ -33,7 +33,6 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
-import androidx.sqlite.db.transaction
 import org.tiqr.data.model.Identity
 import org.tiqr.data.model.IdentityProvider
 import timber.log.Timber
@@ -48,7 +47,7 @@ abstract class TiqrDatabase : RoomDatabase() {
                 Timber.d("Migrating database from version $startVersion to version $endVersion.")
                 Timber.d("Adds 2 columns.")
 
-                database.transaction {
+                database.run {
                     execSQL("ALTER TABLE identity ADD COLUMN showFingerPrintUpgrade INTEGER NOT NULL DEFAULT 1;")
                     execSQL("ALTER TABLE identity ADD COLUMN useFingerPrint INTEGER NOT NULL DEFAULT 0;")
                 }
@@ -60,7 +59,7 @@ abstract class TiqrDatabase : RoomDatabase() {
                 Timber.d("Migrating database from version $startVersion to version $endVersion.")
                 Timber.d("Changes LOGO from BLOB to TEXT.")
 
-                database.transaction {
+                database.run {
                     execSQL("CREATE TABLE new_identityprovider (_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, displayName TEXT NOT NULL, identifier TEXT NOT NULL, authenticationUrl TEXT NOT NULL, ocraSuite TEXT NOT NULL, infoUrl TEXT, logo TEXT);")
                     execSQL("INSERT INTO new_identityprovider (_id, displayName, identifier, authenticationUrl, ocraSuite, infoUrl) SELECT _id, displayName, identifier, authenticationUrl, ocraSuite, infoUrl FROM identityprovider;")
                     execSQL("DROP TABLE identityprovider;")
@@ -71,12 +70,12 @@ abstract class TiqrDatabase : RoomDatabase() {
 
         // From version 8 onwards Room is being used
 
-        val FROM_7_TO_8: Migration = object : Migration(7,8) {
+        val FROM_7_TO_8: Migration = object : Migration(7, 8) {
             override fun migrate(database: SupportSQLiteDatabase) {
                 Timber.d("Migrating database from version $startVersion to version $endVersion.")
                 Timber.d("Adds FK's and indexes.")
 
-                database.transaction {
+                database.run {
                     execSQL("CREATE TABLE new_identity (_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, displayName TEXT NOT NULL, identifier TEXT NOT NULL, identityProvider INTEGER NOT NULL, blocked INTEGER NOT NULL DEFAULT 0, sortIndex INTEGER NOT NULL, showFingerPrintUpgrade INTEGER NOT NULL DEFAULT 1, useFingerPrint INTEGER NOT NULL DEFAULT 0, FOREIGN KEY(identityProvider) REFERENCES identityprovider(_id) ON UPDATE NO ACTION ON DELETE CASCADE);")
                     execSQL("INSERT INTO new_identity (_id, displayName, identifier, identityProvider, blocked, sortIndex, showFingerPrintUpgrade, useFingerPrint) SELECT _id, displayName, identifier, identityProvider, blocked, sortIndex, showFingerPrintUpgrade, useFingerPrint FROM identity;")
                     execSQL("DROP TABLE identity;")
@@ -94,12 +93,12 @@ abstract class TiqrDatabase : RoomDatabase() {
             }
         }
 
-        val FROM_8_TO_9: Migration = object : Migration(8,9) {
+        val FROM_8_TO_9: Migration = object : Migration(8, 9) {
             override fun migrate(database: SupportSQLiteDatabase) {
                 Timber.d("Migrating database from version $startVersion to version $endVersion.")
                 Timber.d("Renames fingerprint to biometric. Renames indexes.")
 
-                database.transaction {
+                database.run {
                     execSQL("CREATE TABLE new_identity (_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, displayName TEXT NOT NULL, identifier TEXT NOT NULL, identityProvider INTEGER NOT NULL, blocked INTEGER NOT NULL DEFAULT 0, sortIndex INTEGER NOT NULL, biometricInUse INTEGER NOT NULL DEFAULT 0, biometricOfferUpgrade INTEGER NOT NULL DEFAULT 1, FOREIGN KEY(identityProvider) REFERENCES identityprovider(_id) ON UPDATE NO ACTION ON DELETE RESTRICT)")
                     execSQL("INSERT INTO new_identity (_id, displayName, identifier, identityProvider, blocked, sortIndex, biometricInUse, biometricOfferUpgrade) SELECT _id, displayName, identifier, identityProvider, blocked, sortIndex, useFingerPrint, showFingerPrintUpgrade FROM identity;")
                     execSQL("DROP TABLE identity;")
@@ -112,7 +111,7 @@ abstract class TiqrDatabase : RoomDatabase() {
                     execSQL("INSERT INTO new_identityprovider (_id, displayName, identifier, authenticationUrl, ocraSuite, infoUrl, logo) SELECT _id, displayName, identifier, authenticationUrl, ocraSuite, infoUrl, logo FROM identityprovider;")
                     execSQL("DROP TABLE identityprovider;")
                     execSQL("ALTER TABLE new_identityprovider RENAME TO identityprovider;")
-                    execSQL("CREATE UNIQUE INDEX index_identityprovider_identifier ON identityprovider(identifier);")
+                    execSQL("CREATE INDEX index_identityprovider_identifier ON identityprovider(identifier);")
                 }
             }
         }
