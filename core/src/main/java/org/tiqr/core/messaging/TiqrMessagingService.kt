7f1eb56fd8544eb01775010c638af8b3a06c5c51
Peter Havekes <peter@havekes.eu>
2023-01-27 12:00:28 +0100

    Remove large icon from PN

--- core/src/main/java/org/tiqr/core/messaging/TiqrMessagingService.kt
+++ core/src/main/java/org/tiqr/core/messaging/TiqrMessagingService.kt
@@ -115,7 +115,6 @@ class TiqrMessagingService : FirebaseMessagingService() {
 
             NotificationCompat.Builder(this, CHANNEL_ID)
                 .setContentIntent(PendingIntent.getActivity(this, 0, intent, flags))
-                .setLargeIcon(AppCompatResources.getDrawable(this, R.drawable.ic_notification_large)?.toBitmap())
                 .setSmallIcon(R.drawable.ic_notification)
                 .setAutoCancel(true)
                 .setWhen(System.currentTimeMillis())
